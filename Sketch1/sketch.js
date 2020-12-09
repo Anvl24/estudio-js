c94f0f2e3f506c5918a66a0915dcf5b7dafd0a4a
Anvl24 <75633188+Anvl24@users.noreply.github.com>
2020-12-08 19:31:22 -0500

    Add files via upload

---
+++ Sketch1/sketch.js
@@ -0,0 +1,61 @@
+//this is the controller sketch, it just takes input and sends it to the secondsketch.js
+var sketch = function (a){
+  let x=0;
+  let y=50;
+  
+  var mic;
+  var amp;
+  
+  a.setup = function() {
+    
+    canvas1 = a.createCanvas(windowWidth,windowHeight) ;
+   registerServiceWorker('service-worker.js')
+  
+  mic = new p5.AudioIn();
+  mic.start();
+  amp = new p5.Amplitude();
+  amp.setInput(mic);
+  a.background(0);
+}
+
+ a.draw = function () {
+  
+  var level = amp.getLevel();
+  a.stroke(255,50);
+  a.ellipse(x, y, level * a.width / 2, level * a.width / 2);
+  a.fill(255,10);
+  //a.console.log(vol);
+   x += 2;
+
+  if(x > a.width){
+    x = 0;
+    y += 50;
+  }
+
+  if(y > a.height){
+    y = 0;
+  }
+}
+}
+var myp51= new p5(sketch);
+
+
+function draw(){
+  background(100)
+
+  //try moving this and the sendMessage to the draw() loop!
+
+  //only send if the mouse is pressed
+  if(mouseIsPressed){
+    //pack up the mouse data into JSON
+    let mouseData = {
+      "x": mouseX,
+      "y": mouseY
+    }
+
+    //send ot to the secondsketch.js
+    sendMessage(mouseData)
+
+  }
+
+}
